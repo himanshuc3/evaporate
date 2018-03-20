cf793de868688330a5473dc6883c7ee220120135
himanshuc3 <himichhabra14@gmail.com>
2018-03-20 22:34:27 +0530

    Initial Commit

---
+++ src/evaporate.js
@@ -0,0 +1,14 @@
+window.vapor = (function () {
+    'use strict';
+    function vapor (els) {
+         
+    }
+     
+    var vapor = {
+        get: function (selector) {
+         
+        }   
+    };
+     
+    return vapor;
+}());
