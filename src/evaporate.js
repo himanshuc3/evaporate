d9eb70756b2f2891f0c1d678460ebf434090e203
himanshuc3 <himichhabra14@gmail.com>
2018-03-21 12:13:22 +0530

    r

--- src/evaporate.js
+++ src/evaporate.js
@@ -1,14 +1,29 @@
-window.vapor = (function () {
-    'use strict';
-    function vapor (els) {
-         
-    }
-     
-    var vapor = {
-        get: function (selector) {
-         
-        }   
+var Vapor = (function() {
+
+    return {
+        makeToast: function () {
+            this.html = `<div class="md-modal md-effect-1" id="modal-1">
+            <div class="md-content">
+                <h3>Modal Dialog</h3>
+                <div>
+                    <p>This is a modal window. You can do the following things with it:</p>
+                    <ul>
+                        <li><strong>Read:</strong> Modal windows will probably tell you something important so don't forget to read what it says.</li>
+                        <li><strong>Look:</strong> modal windows enjoy a certain kind of attention; just look at it and appreciate its presence.</li>
+                        <li><strong>Close:</strong> click on the button below to close the modal.</li>
+                    </ul>
+                    <button class="md-close">Close me!</button>
+                </div>
+            </div>
+        </div>`;
+            $("body").append(this.html);
+            console.log("one");
+        },
+
+        func2: function () {
+            alert("This is func2");
+        }
+
     };
-     
-    return vapor;
-}());
+})();
+
